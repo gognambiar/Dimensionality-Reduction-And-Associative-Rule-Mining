fe8dd11cbb0010a627b0533ab8e60fe38a341ca4
Moonis Javed <monis.javed@gmail.com>
2017-09-23 16:00:31 -0400

    Completed template matching

--- Code/Apriori/Apriori.py
+++ Code/Apriori/Apriori.py
@@ -1,5 +1,6 @@
 import numpy, os, sys, getopt, itertools as it
 import re
+from pprint import pprint
 
 confidence = 70
 # support = .30 #40, 50, 60, 70
@@ -8,7 +9,7 @@ F1Set = {}
 main_data = {}
 rule=[]
 head=[]
-tail=[]
+body=[]
 conf=[]
 supp=[]
 
@@ -16,37 +17,38 @@ def readFile(fileName):
     data = open(fileName).read().split("\n")[:-1]
     data = [i.replace("\r","").split("\t") for i in data]
     for i in xrange(len(data)):
-        for j in xrange(len(data[i])-1):
-            data[i][j] = "G%s_%s" % (str(j+1),data[i][j])
-            if data[i][j] in F1Set:
-                F1Set[data[i][j]] += 1
-            else:
-                F1Set[data[i][j]] = 1
+        for j in xrange(len(data[i])):
 
-    # print F1Set
+            if j < len(data[i]) - 1:
+                data[i][j] = "G%s_%s" % (str(j+1),data[i][j])
+
+            if frozenset([data[i][j]]) not in FSet:
+                FSet[frozenset([data[i][j]])] = 0
+
+            FSet[frozenset([data[i][j]])] += 1
+
+        data[i] = frozenset(data[i])
+
+
+    # pprint(FSet)
+    # exit(0)
     return data
 
 def getFrequentItemSets(data):
     # print(len(data))
-    for key, value in F1Set.items():
+    for key, value in FSet.items():
         if ((value*100)/float(len(data))) < support*100 :
-            F1Set.pop(key, None)
+            FSet.pop(key, None)
         else:
             # print key,value,(value/float(len(data))),(value/float(len(data))) < support,support
-            main_data[frozenset([key])] = value
-            FSet[key] = value
+            main_data[key] = value
+            # FSet[frozenset([key])] = value
 
 def getSupport(data, items):
     count=0
-    for row in data:
-        flag=True
-        for i in items:  
-            if i not in row:
-                flag= False
-                break
-        if flag is True: 
-            count+=1
-    return count
+    # for row in data:
+    #     count += 1&items.issubset(row)
+    return ([items.issubset(row) for row in data]).count(True)
 
 
 def joinItemSets(data):
@@ -54,28 +56,31 @@ def joinItemSets(data):
     # itemList=list(sorted(FSet.keys(), key = lambda x: int("".join(re.findall("\d+",x)))))
     # item1List=list(sorted(F1Set.keys(), key = lambda x: int("".join(re.findall("\d+",x)))))
     itemList = list(FSet.keys())
-    item1List = list(F1Set.keys())
 
     # print len(itemList)
     FSet = {}
     items=[]
     # for item in list(it.combinations(itemList,2)):
     for item in itemList:
-        # end_num = re.findall("\d+",item)[-1]
-        end_num = 0
-        for elem in item1List[(2*int(end_num)):]:
+        for elem in itemList:
+            if item == elem:
+                continue
+
         # if ",".join(item[0].split(",")[:-1]) == ",".join(item[1].split(",")[:-1]):
-            item_str=",".join([item,elem])
-            order = [int(i) for i in re.findall("\d+",item_str)]
+            # item_str=",".join([item,elem])
+            # order = [int(i) for i in re.findall("\d+",item_str)]
+            order = frozenset(list(item) + list(elem))
             # if order != sorted(order) or len(set(order)) != len(order):
-            if len(set(order)) != len(order):
+            if len(order) != len(item) + 1:
                 continue
             # print item_str,order
-            items=item_str.split(",")
-            count = getSupport(data,items)
+            count = getSupport(data,order)
+            # print count,order
             if ((count*100)/float(len(data))) >= support*100 :
-                FSet[item_str] = count
-                main_data[frozenset(item_str.split(","))] = count
+                # print order
+                FSet[order] = count
+                main_data[order] = count
+                # print order
 
 
 def generateRules(data, elem, confidence):
@@ -85,26 +90,115 @@ def generateRules(data, elem, confidence):
         # print elem
         while length > 0:
             for comb in map(frozenset,it.combinations(elem,length)):
-                elemHead = comb
-                supportHead = getSupport(data, elemHead)
-                elemTail = elem.difference(comb)
+                elemBody = comb
+                supportHead = getSupport(data, elemBody)
+                elemHead = elem.difference(comb)
                 elemConfidence = main_data[elem]/float(supportHead)
-                # print '{%s} -> {%s} , confidence = %s, support = %s' % (", ".join(elemHead), ", ".join(elemTail), elemConfidence, main_data[elem])
+                # print '{%s} -> {%s} , confidence = %s, support = %s' % (", ".join(elemBody), ", ".join(elemHead), elemConfidence, main_data[elem])
                 if elemConfidence >= confidence:
                     rule.append(elem)
                     head.append(elemHead)
-                    tail.append(elemTail)
+                    body.append(elemBody)
                     conf.append(elemConfidence)
                     supp.append(main_data[elem])
             length -= 1
 
     else:
+        # print elem
+
         if main_data[elem] >= confidence * 100:
             rule.append(elem)
-            head.append([])
-            tail.append(elem)
+            head.append(elem)
+            body.append([])
             conf.append(main_data[elem]/len(data))
             supp.append(main_data[elem])
+
+def template1(checkType, countType, elems):
+    checkArea = eval(checkType.lower())
+    trueRetData = []
+    falseRetData = []    
+    tempCount = 0
+
+    for i in xrange(len(checkArea)):
+        # print item, elems
+        storeItem = '{%s} -> {%s}' % (", ".join(body[i]), ", ".join(head[i]))
+        tempCount = ([frozenset([j]).issubset(checkArea[i]) for j in elems]).count(True)
+        if tempCount > 0:
+            flag = True
+            # print storeItem,tempCount
+            trueRetData.append([storeItem,tempCount])
+        else:
+            falseRetData.append(storeItem)
+
+    if countType == 'NONE':
+        return (falseRetData,len(falseRetData))
+
+    elif countType == 'ANY':
+        trueRetData = [i[0] for i in trueRetData]
+        return (trueRetData,len(trueRetData))
+
+    else:
+        trueRetData = [i[0] for i in trueRetData if i[1] == countType]
+        return (trueRetData, len(trueRetData))
+
+def template2(checkType, countType):
+    checkArea = eval(checkType.lower())
+    retData = []
+
+    for i in xrange(len(checkArea)):
+        storeItem = '{%s} -> {%s}' % (", ".join(body[i]), ", ".join(head[i]))
+        if len(checkArea[i]) == countType:
+            retData.append(storeItem)
+
+    return (retData,len(retData))
+
+def template3(*kwargs):
+    kwargs = list(kwargs)
+    checkType = kwargs[0]
+
+    data = []
+
+    del kwargs[0]
+    numbers = [int(i) for i in re.findall("\d+",checkType)]
+    logicOper = re.findall("[a-zA-Z]+", checkType)[0]
+
+    # print logicOper
+
+    for number in numbers:
+        argsCount = 4 - number
+
+        funcArgs = [repr(i) for i in kwargs[:(argsCount)]]
+
+        # for arg in funcArgs:
+        #     print repr(arg)
+
+        del kwargs[:(argsCount)]
+
+        tempCall = '''template%s(%s)''' % (number, ", ".join(map(str,funcArgs)))
+
+        # print tempCall
+
+        (result,cnt) = eval(tempCall)
+
+        data.append(result)
+
+    if logicOper == 'or':
+        data = list(set(data[0]).union(set(data[1])))
+
+    else:
+        data = list(set(data[0]).intersection(set(data[1])))
+
+
+    return (data,len(data))
+
+
+
+
+
+
+
+            
+
 
 
 
@@ -128,25 +222,65 @@ def main(argv):
             confidence = float(arg)
         elif opt in ("-s", "--svalue"):
             support = float(arg)
-    print('Input file is ', inputfile)
+    # print('Input file is ', inputfile)
     #print('Output file is ', outputfile)
     #print('Confidence is ', confidence)
-    print('support is ' , support)
+    # print('support is ' , support)
     data = readFile(inputfile)
     getFrequentItemSets(data)
-    print len(main_data)
+    # print len(main_data)
     for i in xrange(len(data[0])-1):
         prev_len = len(main_data)
         joinItemSets(data)
-        print len(main_data)
+        # print len(main_data)
         if prev_len == len(main_data):
             break
 
+    print 'Support is set to %s%%' % (support * 100)
+    items = {}
+    for rul in main_data:
+        length = len(rul)
+        if length not in items:
+            items[length] = 0
+        items[length] += 1
+
+    # print main_data
+
+    for item in sorted(items):
+        print 'Number of length-%s frequent itemsets: %s' % (item, items[item])
+
+    print 'Total number of frequent sets: %s' % (len(main_data))
+
     for elem in main_data:
+        # print elem
         generateRules(data, elem, confidence)
 
-    for i in xrange(len(rule)):
-        print '{%s} -> {%s} , confidence = %s, support = %s' % (", ".join(sorted(head[i])), ", ".join(sorted(tail[i])), conf[i], supp[i])
+
+    (result,cnt) = template1("BODY", "ANY", ['G10_Down'])
+    print '*******************template1 check**************************'
+
+    for res in result:
+        print res
+    print cnt
+
+    (result,cnt) = template2('HEAD',2)
+    print '*******************template2 check**************************'
+
+    for res in result:
+        print res
+    print cnt
+
+    (result,cnt) = template3("1and2", "BODY", "ANY", ['G10_Down'], "HEAD", 2)
+    print '*******************template2 check**************************'
+
+    for res in result:
+        print res
+    print cnt
+
+    # for i in xrange(len(rule)):
+        # print ", ".join(rule[i])
+        # print rule[i]
+        # print '{%s} -> {%s} , confidence = %s, support = %s' % (", ".join(body[i]), ", ".join(head[i]), conf[i], supp[i])
 
     # for i in main_data:
         # print i
